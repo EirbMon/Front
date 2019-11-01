99cc4b26a6aed8242258285b96e5a4d899a5eecb
dcognet <dcognet@enseirb-matmeca.fr>
2019-11-01 23:41:28 +0100

    requete pour avoir les eirbmons

--- src/middleWare/generateGetEirbmonUrl.js
+++ src/middleWare/generateGetEirbmonUrl.js
@@ -3,5 +3,6 @@
 const apiUrl = process.env.REACT_APP_APIURL;
 
 export default function generateGetEirbmonUrl() {
-    return apiUrl + "api/catalogItem/search";
+    // return apiUrl + "api/catalogItem/search";
+    return "http://localhost:4000/getMyEirbmon"
 }
